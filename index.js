ebc84b5d9fabfbb2b62c5a8f1f827a3b39cc93a1
ThOfficialCoder <sehzademar@outlook.com>
2025-10-01 20:29:56 -0400

    Implemented addding and deleting tasks

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let tasks = [];
+
 function updateDateTime() {
     const now = new Date();
     const date = now.toLocaleDateString(undefined, {
@@ -14,6 +16,50 @@ function updateDateTime() {
     });
     document.getElementById('currentDateTime').textContent = date + " | " + time;
 }
+
+document.getElementById('taskForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    addTask();
+});
+
+function addTask() {
+    const taskForm = document.getElementById('taskForm');
+    const task = {
+        task: taskForm.task.value,
+        time: taskForm.time.value,
+        duration: taskForm.duration.value
+    };
+    tasks.push(task);
+    taskForm.reset();
+    displayTasks();
+}
+
+function displayTasks() {
+    const taskList = document.getElementById('taskList');
+    taskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task-item');
+        taskItem.innerHTML = `
+            <p><strong>${task.task}</strong></p>
+            <p>${task.time}</p>
+            <p>${task.duration}</p>
+            <button class="delete-button" data-index=index="${index}">Delete</button>
+        `;
+        taskList.appendChild(taskItem);
+    });
+
+    document.querySelectorAll('.delete-button').forEach(button => {
+        button.addEventListener('click', function() {
+            const index = this.getAttribute('data-index');
+            tasks.splice(index, 1);
+            displayTasks();
+        })
+    })
+}
+
+displayTasks();
 
 updateDateTime();
 setInterval(updateDateTime, 1000);
